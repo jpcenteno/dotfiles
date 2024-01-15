10efce81e3e6613d05c6207bc2a7ba3bd37b3ae3
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-01-15 03:24:58 -0300

    Add some desktop apps for wayland

--- home.nix
+++ home.nix
@@ -42,6 +42,11 @@ rec {
     # Productivity tools:
     pkgs.taskwarrior
 
+    # Desktop environment
+    pkgs.wob
+    pkgs.brightnessctl
+    pkgs.zathura
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
