25282c2766ddf2acd59e6e30399a8f60e6921374
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-01-15 04:24:43 -0300

    Refactor flake.nix for readability

--- flake.nix
+++ flake.nix
@@ -15,18 +15,18 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."j" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      packages.${system} = {
+        default = home-manager.defaultPackage.${system};
+        homeConfigurations."j" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
       };
-
-      inherit home-manager;
-      inherit (home-manager) packages;
     };
 }
